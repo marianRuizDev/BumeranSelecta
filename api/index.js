03dd593899a4524b592fc1639152fe15816fe2e6
Lorenzo <lorenzoolmedo.99@gmail.com>
2022-06-22 15:16:12 -0300

    confuguracion server

---
+++ api/index.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const app = express();
+const db = require("./config/db");
+const cors = require("cors");
+const routes = require("./routes/index");
+const volleyball = require("volleyball");
+const cookieParser = require("cookie-parser");
+const sessions = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const User = require("./models/User");
+require("dotenv").config();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(volleyball);
+
+app.use(cookieParser());
+
+app.use(
+  sessions({
+    secret: "tmdb",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+    },
+    function (email, password, done) {
+      User.findOne({ where: { email } })
+        .then((user) => {
+          if (!user) {
+            return done(null, false);
+          }
+          user.hash(password, user.salt).then((hash) => {
+            if (hash !== user.password) {
+              return done(null, false);
+            }
+            return done(null, user);
+          });
+        })
+        .catch(done);
+    }
+  )
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findByPk(id)
+    .then((user) => {
+      done(null, user);
+    })
+    .catch(done);
+});
+
+app.use("/api", routes);
+
+db.sync({ force: false }).then(() => {
+  app.listen(3001, () => console.log("Escuchando en el puerto 3001"));
+});
