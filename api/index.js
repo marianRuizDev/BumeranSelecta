ad0103f0cc7b9856813d2152539b626a75fa4a9b
Lorenzo <lorenzoolmedo.99@gmail.com>
2022-06-29 12:43:27 -0300

    cosas

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ const sessions = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const Recruiter = require("./models/Recruiter");
-const db = require("../config/db");
+const db = require("./config/db");
 
 app.use(cors());
 
