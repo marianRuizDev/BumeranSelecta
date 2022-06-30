241b850597b817612697fcf3d7cd683b18982a06
Lorenzo <lorenzoolmedo.99@gmail.com>
2022-06-30 09:50:08 -0300

    no hay

--- api/config/db.js
+++ api/config/db.js
@@ -2,7 +2,7 @@ const Sequelize = require("sequelize");
 
 const { DB_USER, DB_HOST, DB_PASSWORD } = process.env;
 
-const db = new Sequelize("bumeranSelecta", "root", "root", {
+const db = new Sequelize("bumeranSelecta", "root", "password", {
   host: DB_HOST,
   dialect: "mysql",
   logging: false,
