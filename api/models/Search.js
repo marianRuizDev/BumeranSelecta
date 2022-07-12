12122ea3e4149bd476ce567decafdf91914af5eb
mariangelkruiz <mariangel8ruiz@gmail.com>
2022-07-12 10:41:17 -0300

    campos nuevos en searches

--- api/models/Search.js
+++ api/models/Search.js
@@ -9,25 +9,40 @@ Search.init(
   {
     position: {
       type: S.STRING,
+      allowNull: false,
     },
     description: {
       type: S.TEXT,
+      allowNull: false,
     },
     vacancies: {
       type: S.INTEGER,
+      allowNull: false,
     },
     time: {
       type: S.DATEONLY,
+      allowNull: false,
     },
     jobSchedules: {
       type: S.STRING,
+      allowNull: false,
     },
     salary: {
       type: S.INTEGER,
+      allowNull: false,
     },
     title: {
       type: S.STRING,
+      allowNull: false,
     },
+    candidates: {
+      type: S.STRING,
+      allowNull: true,
+    },
+    ratingRecruiter: {
+      type: S.STRING,
+      allowNull: true,
+    }
   },
   {
     sequelize: db,
