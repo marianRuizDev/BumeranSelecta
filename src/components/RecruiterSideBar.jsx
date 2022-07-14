2dd1985400e021f7aadff1cb9db612aee8c102ef
Lorenzo <lorenzoolmedo.99@gmail.com>
2022-07-14 10:02:33 -0300

    last fixes

--- src/components/RecruiterSideBar.jsx
+++ src/components/RecruiterSideBar.jsx
@@ -11,7 +11,7 @@ import { addAvtiveSearches } from "../redux/modifyActiveSearches";
 import { assignRecruiterToSearch } from "../redux/assignRecruiter";
 import { getOneUpDate } from "../redux/search";
 
-export default function RecruiterSideBar({ recruiter, search }) {
+function RecruiterSideBar({ recruiter, search }) {
   const { id } = useParams();
   const dispatch = useDispatch();
   const date = new Date().toISOString().split("T")[0];
@@ -68,3 +68,5 @@ export default function RecruiterSideBar({ recruiter, search }) {
     </>
   );
 }
+
+export default RecruiterSideBar;
